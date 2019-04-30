caef039a174d9807e85d503fc78882a745f70442
abheek9 <abheek@wharton.upenn.edu>
2019-04-29 20:03:18 -0700

    got it working! check out the small square

--- CameraOpus/CameraOpus/ViewController.swift
+++ CameraOpus/CameraOpus/ViewController.swift
@@ -24,7 +24,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBAction func takePhoto(_ sender: UIButton) {
     }
     
-    
+    var session = AVCaptureSession()
+    var stillImageOutput: AVCapturePhotoOutput?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
     
     
     @IBOutlet weak var v: UIView!
@@ -35,36 +37,44 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         textInput.delegate = self
-        var session = AVCaptureSession()
-        var stillImageOutput: AVCapturePhotoOutput?
-        var videoPreviewLayer: AVCaptureVideoPreviewLayer?
-        
         do{
+            //We are trying to set the input device to the session ie the back camera
             guard let backCamera =  try AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) else { print("Default video device is unavailable."); return }
             let DeviceInput = try AVCaptureDeviceInput(device: backCamera)
             if session.canAddInput(DeviceInput) {
                 session.addInput(DeviceInput)
                 print("was able to add deviceinput")
+                //Now that we set input device lets set output files
+                let photoOutput = AVCapturePhotoOutput()
+                if session.canAddOutput(photoOutput) {
+                    session.addOutput(photoOutput)
+                    print("was able to set deviceoutput")
+                    //Now we try to connect the preview layer which will eventually be the element in the IB to what the camera sees
+                    videoPreviewLayer = AVCaptureVideoPreviewLayer(session: session)
+                    videoPreviewLayer!.videoGravity =    AVLayerVideoGravity.resizeAspect
+                    videoPreviewLayer!.connection?.videoOrientation =   AVCaptureVideoOrientation.portrait
+                    photoPreviewImageView.layer.addSublayer(videoPreviewLayer!)
+                    print("seems like we have added a subLayer")
+                    session.startRunning()
+                    print("session is running?")
+                }
             }
-            let photoOutput = AVCapturePhotoOutput()
-            if session.canAddOutput(photoOutput) {
-                session.addOutput(photoOutput)
-                print("was able to set deviceoutput")
-            }
-            
         }
         catch{
             print("there must have been an error in vievDidLoad")
             return
         }
-    
     }
     
     func viewWillAppear(){
         //captureSession = AVCaptureSession()
         //captureSession!.sessionPreset = AVCaptureSession.Preset.photo
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        videoPreviewLayer!.frame = previewView.bounds
     }
         
         // Do any additional setup after loading the view.
